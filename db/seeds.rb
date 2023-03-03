1bbab9d40938a70f8a2ae245e51056b5d5ba4589
Anna <annamtoman@gmail.com>
2023-03-03 13:38:34 -0600

    adds model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,12 @@
 song = Song.create(title: "ENERGY", album: "Renaissance", artist: "Beyonce", year: 2022)
 song.save
 
-song = Song.create
+song = Song.create(title: "Formation", album: "Lemonade", artist: "Beyonce", year: 2016)
+song.save
+
+song = Song.create(title: "Bow Down", album: "Homecoming Live", artist: "Beyonce", year: 2019)
+song.save
+
+song = Song.create(title: "Drunk in Love", album: "Beyonce", artist: "Beyonce", year: 2013)
+song.save
+
